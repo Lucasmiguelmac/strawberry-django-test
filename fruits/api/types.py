0d2c393155c7c28208eb3db77f969ec127701c79
Lucas <lucasmiguelmac@gmail.com>
2022-01-31 09:57:01 +0000

    Working nested retrieve

--- fruits/api/types.py
+++ fruits/api/types.py
@@ -1,7 +1,17 @@
 import strawberry_django
 from fruits import models
+from strawberry_django import auto
+from typing import List
+
 
 @strawberry_django.type(models.Fruit)
 class Fruit:
-    name: str
-    color: str
+    id: auto
+    name: auto
+    color: 'Color'
+
+@strawberry_django.type(models.Color)
+class Color:
+    id: auto
+    name: auto
+    fruits: List[Fruit]
--- fruits/models.py
+++ fruits/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 class Fruit(models.Model):
     name = models.CharField(max_length=20)
-    color = models.CharField(max_length=20)
+    color = models.ForeignKey(
+        'Color',
+        related_name='fruits',
+        on_delete=models.CASCADE
+    )
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
